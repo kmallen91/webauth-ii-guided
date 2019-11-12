c8e7655a514eab83f55f8b5166439770d609bd13
Kevin Allen <kevin.allen91@gmail.com>
2019-11-12 10:29:04 -0800

    added cookie and session middleware

--- api/server.js
+++ api/server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors"); // research -- 'credentials: true' and 'withCredentials' when connecting from your React application
+const session = require("express-session");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
+
+const sessionConfiguration = {
+  name: "booger", // default name is sid, using different name to hide library
+  secret: process.env.COOKIE_SECRET || "is it secret? is it safe?",
+  cookie: {
+    maxAge: 1000 * 60 * 60, // valid for 1 hour in milliseconds
+    secure: process.env.NODE_ENV === "development" ? false : true, // do we send cookie over https only? should be true for production
+    httpOnly: true // prevent client javascript code from acesssing the cookie
+  },
+  resave: false, // save sessions even when they have not changed
+  saveUnitialized: true // read about it on the docs to respect GDPR, should default to false in production
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfiguration));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up", session: req.session });
 });
 
 module.exports = server;
